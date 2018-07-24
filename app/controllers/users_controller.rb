0365ca1d9aeacbfcf944934b038ee5eb62697bad
Hajime Yasui <anjineee@gmail.com>
2018-07-24 03:57:35 +0000

    deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
-  end
+    if params[:back]
+      @user = User.new(user_params)
+    else
+      @user = User.new
+    end 
+  end  
+  
   
   def create
     @user = User.new(user_params)
@@ -11,14 +16,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+   @user = User.find(params[:id])
+  end
   
   def show
     @user = User.find(params[:id])
   end
   
+  def update
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+     flash[:success] = "Profileを編集しました！"
+     redirect_to user_path
+   else
+    render 'edit'
+   end 
+  end
+  
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :capacity, :password,
                                  :password_confirmation,:image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
                     before_save { email.downcase! }
                     has_secure_password
-                    validates :password, presence: true, length: { minimum: 6 }
+  validates :capacity,  presence: true, length: { maximum: 255 }                  
+  validates :password, presence: true, length: { minimum: 6 }
                     
   has_many :events
 end
