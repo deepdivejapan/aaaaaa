a382eb28e01b6b89650a2d9bf822492c96d918da
Hajime Yasui <anjineee@gmail.com>
2018-08-15 08:04:08 +0000

    form-update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,9 @@
 
 //= require jquery
 
-
+$(document).on('change', ':file', function() {
+    var input = $(this),
+    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.parent().parent().next(':text').val(label);
+});
